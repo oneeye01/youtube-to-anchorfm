f64ecffd61e962e2dcee47d80feb75c91af6bbab
Oneeye01 <claudioserena01@gmail.com>
2023-03-21 18:27:01 +0100

    Update index.js

--- src/anchorfm-pupeteer/index.js
+++ src/anchorfm-pupeteer/index.js
@@ -109,7 +109,10 @@ async function postEpisode(youtubeVideoInfo) {
     console.log('Logged in');
 
     console.log('Uploading audio file');
-    console.log(document.querySelector('input[type=file]'));
+    await page.evaluate(() => {
+      console.log(document.querySelector('input[type=file]'));
+    });
+    
     await page.waitForFunction("document.querySelector('input[type=file]')");
     /* await page.waitForSelector('input[type=file]'); */
     const inputFile = await page.$('input[type=file]');
